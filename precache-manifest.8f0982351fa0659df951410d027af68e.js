a66e51a5dcfb0e6eec149f7e961ce4a08253e892
Bojan Krstic <krstic.bojan80@gmail.com>
2019-04-05 11:23:15 +0200

    Updates

---
+++ precache-manifest.8f0982351fa0659df951410d027af68e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f52983028e4e5bb8f4a67024b8a868e2",
+    "url": "/fun/static/media/smile.f5298302.jpg"
+  },
+  {
+    "revision": "fa86376212c1ef6a3095",
+    "url": "/fun/static/css/main.aca8ec05.chunk.css"
+  },
+  {
+    "revision": "154d4ed0edf103aeebf2386bef965761",
+    "url": "/fun/static/media/2018russia.154d4ed0.jpg"
+  },
+  {
+    "revision": "217aea3e25e04dca828d",
+    "url": "/fun/static/js/1.217aea3e.chunk.js"
+  },
+  {
+    "revision": "6e0c5e6e7e805ca73c78",
+    "url": "/fun/static/js/runtime~main.6e0c5e6e.js"
+  },
+  {
+    "revision": "7bc1f89db4046731dcfadab9e4f4a2c6",
+    "url": "/fun/static/media/film1.7bc1f89d.jpg"
+  },
+  {
+    "revision": "fa86376212c1ef6a3095",
+    "url": "/fun/static/js/main.fa863762.chunk.js"
+  },
+  {
+    "revision": "17e43de47ea2378a2ee9e1b26f701068",
+    "url": "/fun/static/media/zvezda.17e43de4.jpg"
+  },
+  {
+    "revision": "47a3c6a7bf2de3b9c95b5cf9b2b4e0d1",
+    "url": "/fun/static/media/fica2.47a3c6a7.jpg"
+  },
+  {
+    "revision": "471b3d53037994ebfbed3b53fb13ecf0",
+    "url": "/fun/static/media/car.471b3d53.jpg"
+  },
+  {
+    "revision": "217aea3e25e04dca828d",
+    "url": "/fun/static/css/1.e513e94d.chunk.css"
+  },
+  {
+    "revision": "144738228059c59490506cf14b61b780",
+    "url": "/fun/index.html"
+  }
+];
